2d77364d3824013f4b5a1e0d7f4682f800348ab2
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2019-12-13 15:05:36 -0600

    Update rbac-rules.js

--- well-done/src/rbac-rules.js
+++ well-done/src/rbac-rules.js
@@ -1,5 +1,3 @@
-import Dashboard from "pages/Dashboard";
-
 const rules ={
     visitor:{
         static:["dashboard-page:visit"]
@@ -21,19 +19,38 @@ const rules ={
         static:[
             "dashboard-page:visit",
             "operators:create",
-            "operators:get",
-            "pump-info:get",
-            "sensor-info:get",
+            "operators:list",
+            "pump-info:list",
+            "sensor-info:list",
             "pump-info:create",
             "sensor-info:create"
         ],
+        dynamic:{
+            //update userID and org owner IDs with foreign keys on Russ' new/ or Labs 17 E.Ps
+            "organizations.edit":({userId,orgOwnerId})=>{
+                if (!userId || !orgOwnerId) return false;
+                return userId === orgOwnerId;
+            },
+            "sensor-info.edit":({orgId, sensorOwnerId})=>{
+                if (!orgId || !sensorOwnerId) return false;
+                return orgId === sensorOwnerId;
+            },
+            "pumps.edit":({orgId, pumpOwnerId})=>{
+                if (!orgId || !pumpOwnerId) return false;
+                return orgId === !pumpOwnerId;
+            },
+            "operators.edit":({orgId, opOrgId})=>{
+                if (!orgId || !opOrgId) return false;
+                return orgId === opOrgId;
+            }
+        }
     },
 
     operators:{
         static:[
             "dashboard-page:visit",
             "pump-info:get",
-            "sensor-info:get",
+            "sensor-info:get"
         ]
     }
 };
